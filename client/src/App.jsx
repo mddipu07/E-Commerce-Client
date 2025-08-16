07ad29e006e58cab7d6394fbd4bc6b112839ae6e
Md <dipum0505@gmail.com>
2025-08-16 22:04:03 +0600

    navbar functionality done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import {Routes , Route, useLocation} from "react-router-dom"
+import Home from './pages/Home'
+import Products from './pages/Products'
+import ProductDetails from './pages/ProductDetails'
+import Cart from './pages/Cart'
+import { AppContext } from './Context/AppContext'
+import Navbar from './Components/Navbar'
+import Seller from './pages/Seller'
+import MyOrders from './pages/MyOrders'
+import Auth from './models/Auth'
 const App = () => {
+  const {isSeller , showUserLogin} = useContext(AppContext)
+  const isSellerPath = useLocation().pathname.includes("seller")
   return (
     <div>
-      <h1 className='text-red-500 text-3xl'>Hello World!!</h1>
+      {isSellerPath ? null : <Navbar /> }
+      {
+        showUserLogin ? <Auth /> : null
+      }
+     <div>
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/seller" element={<Seller />} />
+         <Route path="/products" element={<Products />} />
+         <Route path="/products/:id" element={<ProductDetails />} />
+         <Route path="/cart" element={<Cart />} />
+         <Route path="/my-orders" element={<MyOrders />} />
+       </Routes>
+     </div>
     </div>
   )
 }
